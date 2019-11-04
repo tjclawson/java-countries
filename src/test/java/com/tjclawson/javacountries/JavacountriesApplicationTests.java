75a4e004b9e1051e471322d1683565d4f32cc6a7
tjclawson <tjamesclawson@gmail.com>
2019-11-04 16:43:56 -0500

    Initialize project

---
+++ src/test/java/com/tjclawson/javacountries/JavacountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjclawson.javacountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
